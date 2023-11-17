9ff03a0f5eefec8a746b3d86b32573db083afbab
alex <alex.mors@gmx.de>
2023-11-17 11:50:19 +0100

    update

--- src/components/Download.tsx
+++ src/components/Download.tsx
@@ -1,20 +1,37 @@
 "use client";
 
 import React from "react";
+import JSZip from "jszip";
 import { useFileStore } from "~/ts/state";
 
+async function download() {
+  const viewers = document.querySelectorAll("model-viewer");
+
+  const zip = new JSZip();
+  const promises = Array.from(viewers).map(async (v) => {
+    try {
+      const name = v.getAttribute("alt")?.replace(".glb", "");
+      const blob = await v.toBlob({ idealAspect: true });
+      zip.file(`${name}.png`, blob);
+    } catch (error) {
+      // console.log(error);
+    }
+  });
+
+  await Promise.all(promises);
+  const content = await zip.generateAsync({ type: "blob" });
+  const url = URL.createObjectURL(content);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "GLB-Thumbnails.zip";
+  a.click();
+  URL.revokeObjectURL(url);
+}
+
 export const DownloadButton: React.FC = () => {
-  const { download, setDownload } = useFileStore();
   return (
     <button
-      onClick={() => {
-        if (download) {
-          setDownload(false);
-          setDownload(true);
-          return;
-        }
-        setDownload(true);
-      }}
+      onClick={download}
       className="rounded bg-slate-600 px-4 py-2 text-center font-bold text-white hover:bg-violet-900"
     >
       Download Thumbnails
--- src/components/ModelViewer.tsx
+++ src/components/ModelViewer.tsx
@@ -1,61 +1,22 @@
-import { useEffect, useRef, useState } from "react";
-import { useFileStore } from "~/ts/state";
+import { useEffect } from "react";
 
 interface ModelViewerProps {
   name: string;
   path: string;
 }
 
-async function downloadPosterToBlob(name: string, modelViewer: any) {
-  const blob = await modelViewer.toBlob({ idealAspect: true });
-  const url = URL.createObjectURL(blob);
-  const a = document.createElement("a");
-  a.href = url;
-  a.download = `${name}.png`;
-  a.click();
-  URL.revokeObjectURL(url);
-}
-
 export const ModelViewer: React.FC<ModelViewerProps> = ({ name, path }) => {
-  const ref = useRef(null);
-  const [loaded, setLoaded] = useState(false);
-  const { download } = useFileStore();
-
   useEffect(() => {
     import("@google/model-viewer").catch(console.error);
   }, []);
-
-  useEffect(() => {
-    const modelViewer = ref.current;
-
-    if (modelViewer) {
-      modelViewer.addEventListener("load", () => setLoaded(true));
-    }
-
-    return () => {
-      if (modelViewer) {
-        modelViewer.removeEventListener("load", () => setLoaded(true));
-      }
-    };
-  }, []);
-
-  useEffect(() => {
-    if (download && loaded) {
-      downloadPosterToBlob(name, ref.current);
-    }
-  }, [download]);
 
   return (
     <div className="h-64 w-full ">
       <model-viewer
         style={{ width: "100%", height: "100%" }}
-        ref={ref}
         src={path}
+        alt={name}
         shadowIntensity="1"
-        camera-controls
-        auto-rotate
-        cameraOrbit="-56.87deg 72.12deg 17.45m"
-        fieldOfView="30deg"
       ></model-viewer>
     </div>
   );
