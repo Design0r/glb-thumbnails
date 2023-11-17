4e128c6ce29f391648551ecc8863151561f2c96b
alex <alex.mors@gmx.de>
2023-11-17 15:42:35 +0100

    refactor

--- src/components/Download.tsx
+++ src/components/Download.tsx
@@ -32,7 +32,7 @@ export const DownloadButton: React.FC = () => {
   return (
     <button
       onClick={download}
-      className="rounded bg-slate-600 px-4 py-2 text-center font-bold text-white hover:bg-violet-900"
+      className="rounded bg-violet-900 px-4 py-2 text-center font-bold text-white hover:bg-violet-500 "
     >
       Download Thumbnails
     </button>
@@ -45,17 +45,10 @@ export const CreateThumbnailButton: React.FC = () => {
   return (
     <div className="justify-center">
       <button
-        onClick={() => {
-          if (thumbnails) {
-            setThumbnails(false);
-            setThumbnails(true);
-            return;
-          }
-          setThumbnails(true);
-        }}
-        className="rounded bg-slate-600 px-4 py-2 text-center font-bold text-white hover:bg-violet-900"
+        onClick={() => setThumbnails(!thumbnails)}
+        className="rounded bg-violet-900 px-4 py-2 text-center font-bold text-white hover:bg-violet-500 "
       >
-        Create Thumbnails
+        Load Models
       </button>
     </div>
   );
--- src/components/FileDropField.tsx
+++ src/components/FileDropField.tsx
@@ -6,12 +6,6 @@ const FileDropField: React.FC = () => {
   const [dragOver, setDragOver] = useState<boolean>(false);
   const { addFile, addUrl } = useFileStore();
 
-  const onFilesDrop = (files: File[]) => {
-    files.map((f) => {
-      f.name.includes(".glb") && addFile(f);
-    });
-  };
-
   const handleDragEnter = useCallback((e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
     e.stopPropagation();
@@ -40,44 +34,46 @@ const FileDropField: React.FC = () => {
     setDragOver(false);
 
     if (event.dataTransfer.items) {
-      // Use DataTransferItemList interface to access the file(s)
       for (let i = 0; i < event.dataTransfer.items.length; i++) {
-        if (event.dataTransfer.items[i]?.kind === "file") {
-          const file = event.dataTransfer.items[i]?.getAsFile();
-          if (file) {
-            const url = URL.createObjectURL(file);
-            addFile(file);
-            addUrl(url);
-          }
+        if (event.dataTransfer.items[i]?.kind !== "file") {
+          continue;
         }
+
+        const file = event.dataTransfer.items[i]?.getAsFile();
+        if (!file) {
+          continue;
+        }
+
+        if (!file.name.includes(".glb")) {
+          continue;
+        }
+
+        const url = URL.createObjectURL(file);
+        addFile(file);
+        addUrl(url);
       }
     } else {
-      // Use DataTransfer interface to access the file(s)
       for (let i = 0; i < event.dataTransfer.files.length; i++) {
         const file = event.dataTransfer.files[i];
-        if (file) {
-          const url = URL.createObjectURL(file);
-          addFile(file);
-          addUrl(url);
+        if (!file) {
+          continue;
         }
+
+        if (!file.name.includes(".glb")) {
+          continue;
+        }
+
+        const url = URL.createObjectURL(file);
+        addFile(file);
+        addUrl(url);
       }
     }
   };
 
-  const handleFileSelect = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      const selectedFiles = e.target.files;
-      if (selectedFiles) {
-        onFilesDrop(Array.from(selectedFiles));
-      }
-    },
-    [onFilesDrop],
-  );
-
   return (
     <div
       className="h-full  w-full cursor-pointer select-none rounded-lg border-2 border-dashed
-        border-slate-100 bg-opacity-50 p-6 text-center shadow-lg shadow-slate-900" // Fixed width and height classes
+        border-slate-100 bg-opacity-50 p-6 text-center shadow-lg shadow-slate-900"
       onDragEnter={handleDragEnter}
       onDragOver={handleDragOver}
       onDragLeave={handleDragLeave}
@@ -88,7 +84,6 @@ const FileDropField: React.FC = () => {
       ) : (
         <p className="justify-center ">Drag and drop a GLB file here</p>
       )}
-      <input type="file" className="hidden" onChange={handleFileSelect} />
     </div>
   );
 };
